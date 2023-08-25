2440d0dddf814bef795a2471babfa5e336bfddc5
darshan30899 <85397437+darshan30899@users.noreply.github.com>
2023-08-25 15:47:47 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 provider "github" {
-  token = "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"
+token = "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"
 }
 
 
