c2b14f051e4e4e451540828295bcd2e859363ab7
darshan30899 <85397437+darshan30899@users.noreply.github.com>
2023-08-25 15:45:22 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,10 @@
 provider "github" {
-  token = "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"
+  token = "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"
 }
 
 
 resource "github_repository" "you" {
   name        = "example"
   description = "My awesome codebase"
-
   visibility = "public"
 }                                                                                                                                                                                                                              
